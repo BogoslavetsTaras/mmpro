692b87936a85c5b2416b4e70f85ea179a915dfa6
Taras Bohoslavets <bogoslavetstaras@gmail.com>
2023-08-10 23:26:20 +0300

    13

---
+++ src/lessons13streamapi/App.java
@@ -0,0 +1,8 @@
+package lessons13streamapi;
+
+public class App {
+    public static void main ( String[] args ) {
+        System.out.println( "Hello World!" );
+        System.out.println( "Слава Україні!" );
+    }
+}
